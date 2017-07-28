64d93b8a2753861f83acf78693ec69cef27d5856
mahmoudta <30440251+mahmoudta@users.noreply.github.com>
2017-07-28 16:53:01 -0700

    fix

--- final_project/Control.cpp
+++ final_project/Control.cpp
@@ -17,3 +17,21 @@ void Control:: draw(Graphics& g, int x, int y, size_t z) const {
 	_borderDrawer->draw(g, _left + x, _top + y, _width, _height);
 }
 
+void Control::SetVisibility(bool visibility) {
+	_graphics.setCursorVisibility(visibility);
+}
+
+void Control::SetForeground(ForegroundColor color) {
+	//_graphics.setForeground(Color);
+	
+}
+
+void Control::SetBackground(BackgroundColor color) {
+	//this->_background = color;
+
+}
+
+void Control::SetBorder(BorderType border) {
+
+	
+}
