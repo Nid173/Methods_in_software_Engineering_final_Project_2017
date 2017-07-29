ff327b56fcea454e502f39d00ff559b48d6f54d6
mahmoudta <30440251+mahmoudta@users.noreply.github.com>
2017-07-29 02:15:49 -0700

    d

--- final_project/source/main.cpp
+++ final_project/source/main.cpp
@@ -13,17 +13,17 @@ int main()
 {
 
 	Panel panel(20, 100);
-	panel.setLayer(2);
+	panel.setLayer(1);
 	panel.setBackground(Color::Blue);
 	panel.setBorder(BorderType::Double);
 	TextBox textBox1(10);
 	textBox1.setBackground(Color::Green);
-	textBox1.setForeground(Color::Red);
-	textBox1.setText(L"hello");
-	textBox1.setLayer(1);
+	//textBox1.setForeground(Color::Red);
+	textBox1.setText(L"heeeeee");
+	textBox1.setLayer(2);
 	textBox1.setBorder(BorderType::Single);
-	panel.AddControl(textBox1, 4, 4);
-	panel.setFocus(textBox1);
+	panel.AddControl(textBox1, 7, 4);
+	//panel.setFocus(textBox1);
 	EventEngine engine;
 	engine.run(panel);
 	//g.write("hello");
