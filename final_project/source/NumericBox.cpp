8615dc689c8eacdb4af1ebe5ae1af6aa8c06341d
mahmoudta <30440251+mahmoudta@users.noreply.github.com>
2017-07-31 19:00:06 -0700

    change

--- final_project/source/NumericBox.cpp
+++ final_project/source/NumericBox.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "NumericBox.h"
 #include "Button.h"
+#include <string>
 
 
 NumericBox::NumericBox(int width, int min, int max) {
@@ -8,8 +9,9 @@ NumericBox::NumericBox(int width, int min, int max) {
 	_max = max;
 	_width = width;
 	_height = 2;
+	_value = min;
 	Button minus(2);
-	minus.setText(L"-");
+	minus.setText(L" "+to_wstring(_value));
 	minus.setBorder(BorderType::Single);
 	minus.setLeft(this->_left - 1);
 	//minus.addListener(listener);
@@ -53,10 +55,13 @@ int NumericBox::GetValue() {
 void NumericBox::plus() {
 	if (setValue(_value + 1)) {
 	//the value changed
+	//XXX.setText(L" "+to_wstring(_value));
 	}
 }
+
 void NumericBox::minus() {
 	if (setValue(_value - 1)) {
 		//the value changed
+		//XXX.setText(L" "+to_wstring(_value));
 	}
 }
