df0c85c2fc466a1c46dc8f49e5f139f6a2df9d48
adham1011 <adham@megatam.net>
2017-08-02 22:02:22 +0300

    Update MessageBox.cpp

--- final_project/source/MessageBox.cpp
+++ final_project/source/MessageBox.cpp
@@ -1,7 +1,6 @@
 #include"stdafx.h"
 #include "MessageBox.h"
 
-<<<<<<< HEAD
 Messagebox::Messagebox(int width, int height, wstring error):Panel(height+3,width) {
 
 	if (width <= 20) 
@@ -12,17 +11,14 @@ Messagebox::Messagebox(int width, int height, wstring error):Panel(height+3,widt
 	this->setForeground(Color::Black);
 
 
-=======
-Messagebox::Messagebox(string error) {
-	this->setBorder(BorderType::Single);
-	this->setForeground(Color::White);
->>>>>>> a34f18c2c1f097175f091f7e5e89f0c8c7191c97
 	Button* ok = new Button(7);
 	ok->setText(L"OK");
 	this->setBackground(Color::White);
 	ok->setForeground(Color::Green);
 	ok->setBorder(BorderType::Single);
 	_controls.push_back(ok);
+	
+
 	Button* cancel = new Button(7);
 	cancel->setText(L"cancel");
 	this->setBackground(Color::White);
