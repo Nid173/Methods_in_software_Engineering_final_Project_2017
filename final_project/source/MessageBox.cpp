458589cff7ebec6e05a6eb9bf8ed93a42db47a01
adham1011 <adham@megatam.net>
2017-08-02 21:58:05 +0300

    messagebox

--- final_project/source/MessageBox.cpp
+++ final_project/source/MessageBox.cpp
@@ -1,16 +1,19 @@
 #include"stdafx.h"
 #include "MessageBox.h"
 
-Messagebox::Messagebox(string error) {
+Messagebox::Messagebox(int width, int height, wstring error):Panel(height+3,width) {
+
+	if (width <= 20) 
+		_width = 20;
 
 	this->setBorder(BorderType::Single);
-	this->setForeground(Color::White);
+	this->setBackground(Color::White);
+	this->setForeground(Color::Black);
 
 
 	Button* ok = new Button(7);
 	ok->setText(L"OK");
-	ok->setTop(_height-3);
-	ok->setLeft(2);
+	this->setBackground(Color::White);
 	ok->setForeground(Color::Green);
 	ok->setBorder(BorderType::Single);
 	_controls.push_back(ok);
@@ -18,9 +21,10 @@ Messagebox::Messagebox(string error) {
 
 	Button* cancel = new Button(7);
 	cancel->setText(L"cancel");
-	cancel->setTop(7);
-	cancel->setLeft(12);
+	this->setBackground(Color::White);
 	cancel->setForeground(Color::Red);
 	cancel->setBorder(BorderType::Single);
 	_controls.push_back(cancel);
+
+	_message = error;
 }
--- final_project/source/MessageBox.h
+++ final_project/source/MessageBox.h
@@ -1,14 +1,36 @@
 #pragma once
 #include "Panel.h"
 
-class Ok :public MouseListener {
-public:
-	virtual void draw(Control &control, int	x, int y, bool isleft) {}
-};
 
 class Messagebox : public Panel {
-	string _message;
+	wstring _message;
+
 public:
-	Messagebox(string error);
+	Messagebox(int width,int height ,wstring error);
+	inline void setOkListener(MouseListener& ok);
+	inline void setCancelListener(MouseListener& cancel);
+	inline virtual void setTop(int top);
+	inline  virtual void setLeft(int left);
+
 };
 
+void Messagebox::setOkListener(MouseListener& ok) {
+	Button* b = static_cast<Button*>(_controls[0]);
+	b->addListener(ok);
+ }
+void Messagebox::setCancelListener(MouseListener& cancel) {
+	Button* b = static_cast<Button*>(_controls[1]);
+	b->addListener(cancel);
+
+ }
+
+void Messagebox:: setTop(int top) {
+	_top = top;
+	_controls[0]->setTop(top+_height-1);
+	_controls[1]->setTop(top + _height - 1);
+  }
+  void Messagebox::setLeft(int left) {
+	  _left = left;
+	  _controls[0]->setLeft(left + 2);
+	  _controls[1]->setLeft(left + _width - 2);
+  }
