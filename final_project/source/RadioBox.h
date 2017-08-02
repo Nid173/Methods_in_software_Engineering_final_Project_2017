65e76623dfd22a61edea1795b3f427dcc7330d2a
mahmoudta <30440251+mahmoudta@users.noreply.github.com>
2017-08-02 03:23:03 -0700

    form

--- final_project/source/RadioBox.h
+++ final_project/source/RadioBox.h
@@ -8,7 +8,7 @@ public:
 	RadioBox(int width, int height, vector<string> entries);
 	~RadioBox() {}
 	virtual string className() { return "RadioBox"; }
-	void click(size_t index);
+	virtual void click(size_t index);
 	size_t GetSelectedIndex();
 	void SetSelectedIndex(size_t index);
 };
